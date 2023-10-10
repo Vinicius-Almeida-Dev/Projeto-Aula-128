301f356f97ff837d50dc2eb8bb12baa2ec1da15e
Vinicius-Almeida-Dev <viniciussalne@hotmail.com>
2023-10-10 12:52:53 -0300

    Alterei o nome da classe

---
+++ Entities/Department.cs
@@ -0,0 +1,16 @@
+﻿namespace Projeto_Aula_128.Entities
+{
+    internal class Department
+    {
+        public string Name { get; set; }
+
+        public Department()
+        {
+        }
+
+        public Department(string name)
+        {
+            Name = name;
+        }
+    }
+}
