893fd67554de2bace1a1d9594ab8ff4820bcb4f5
Vinicius-Almeida-Dev <viniciussalne@hotmail.com>
2023-10-10 12:46:01 -0300

    Criada nova pasta no projeto, e adicionada a class: Departament.cs.. Começando o projeto.

---
+++ Entities/Departament.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_Aula_128.Entities
+{
+    internal class Departament
+    {
+    }
+}
