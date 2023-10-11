587f031bcb25baf81e35146b2470ec6d29af4e1f
Vinicius-Almeida-Dev <viniciussalne@hotmail.com>
2023-10-11 08:20:15 -0300

    Leve ajuste (program.cs)

--- Program.cs
+++ Program.cs
@@ -24,9 +24,10 @@ namespace Projeto_Aula_128
             double Salary =  double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
             Department Dept = new Department(deptName);
+            Console.WriteLine(Dept.Name);
             Worker worker = new Worker(Name, Lvl, Salary, Dept);
 
-            Console.Write("How many contracts to this worker?");
+            Console.Write("How many contracts to this worker? ");
             int N = int.Parse(Console.ReadLine());
             for (int i = 1; i <= N; i++)
             {
